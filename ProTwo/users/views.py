df8c68ca802492ac7cf66c6efe39c7575f0fe69b
Gabriel <ionescuig@yahoo.com>
2018-10-15 19:10:08 +0100

    Add files via upload

--- ProTwo/users/views.py
+++ ProTwo/users/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import User
 
 def users_list(request):
-    qs = User.objects.all()
+    qs = User.objects.all().order_by('first_name')
     return render(request, 'users/users_list.html', {'users_list': qs})
